f4d8c65127e1a0668c13dd8f94acddaef223af59
Ludfor Energia - Samuel De Moura Souza <samuel.moura@ludfor.com.br>
2025-06-03 15:28:44 -0300

    primeiro commit do site Seja Igreja

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6861ec',
+        secondary: '#2684eb'
+      }
+    },
+  },
+  plugins: [],
+}
