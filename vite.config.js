95100e4f39b359558b5dc51ed63b3f444c0f0341
Site Seja igreja <sejaigreja.midias@gmail.com>
2025-06-03 19:05:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // <- IMPORTANTE: deixe string vazia ou '/' para Netlify
+  base: '/', // <- ESSENCIAL PARA NETLIFY!
 })
